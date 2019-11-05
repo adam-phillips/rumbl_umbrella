d41c4c73a8d50f91398bd4e6474e8c97a0ae89b5
Adam Phillips <adam.phillips@dockyard.com>
2019-11-04 19:59:54 -0500

    Build GenServer
    Instead of manual work in the original counter, convert it to a
    GenServer to handle splitting client and server code, set up references
    for synchronous messages, and control the receive loop

--- apps/info_sys/lib/info_sys/counter.ex
+++ apps/info_sys/lib/info_sys/counter.ex
@@ -1,46 +1,45 @@
 defmodule InfoSys.Counter do
-  def inc(pid), do: send(pid, :inc)
-
-  def dec(pid), do: send(pid, :dec)
+  use GenServer
 
   @doc """
-    Send a request for the value of the counter by creating a unique reference,
-    and send a message to the counter with the command, pid, and reference.
-    Await a response matching the exact ref and if there is no match then exit
-    the current process with :timeout
+    GenServer `cast` is used to send an asynchronous message to increment or
+    decrement the counter
   """
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref() # make_ref/0 is an Erlang function
-    send(pid, {:val, self(), ref})
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-    receive do
-      {^ref, val} -> val
-    after
-      timeout -> exit(:timeout)
-    end
+  def dec(pid), do: GenServer.cast(pid, :dec)
+
+  @doc """
+    Use GenServer to send synchronous messages that return the server state
+  """
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
 
   @doc """
-    Required by OTP; accept the initial state of the counter, spawn a process,
-    and return :ok and the pid identifying the newly spawned process. The
-    spawned process calls the private `listen` function, listening for
-    messages to process
+    Start a GenServer, give it the current module name, and the counter. This
+    spawns a new process and invokes InfoSys.Counter.init to set up initial
+    state
   """
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
+  @doc """
+    Handle cast for :inc or :dec to increment or decrement accordingly
+  """
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
 
-      :dec ->
-        listen(val - 1)
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
 
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
-        listen(val)
-    end
+  @doc """
+    Handle :val and specify the return value.
+  """
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
